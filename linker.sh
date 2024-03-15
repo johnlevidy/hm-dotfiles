2a0c3a88d07985747f6119e9eb29a4d09120a682
John <john.levidy@gmail.com>
2024-03-14 20:23:45 -0400

    add nvim.nix, trim the linker, should only need home manager entry point

---
+++ linker.sh
@@ -0,0 +1,1 @@
+ln -s /home/john/hm-dotfiles/home-manager.nix /etc/nixos/home-manager.nix
---
+++ nvim.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, lib, ... }:
+let
+  myHop = pkgs.vimUtils.buildVimPlugin {
+    name = "myHop";
+    src = /home/john/Code/hop.nvim;
+  };
+in
+{
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      nvim-treesitter
+      nvim-treesitter.withAllGrammars
+      lualine-nvim
+      guess-indent-nvim
+      gruvbox-nvim
+      # telescope
+      telescope-nvim
+      plenary-nvim
+      telescope-fzf-native-nvim
+      # all for autocomplete
+      vim-vsnip
+      lspkind-nvim
+      cmp-nvim-lsp
+      cmp-vsnip
+      cmp-path
+      nvim-cmp
+      nvim-lspconfig
+      # surround with ys, delete with ds
+      nvim-surround
+      # file tree
+      nvim-tree-lua
+      # create missing directories on file save
+      mkdir-nvim
+      # better navigation
+      # hop-nvim
+      myHop
+      # symbolic outlines
+      symbols-outline-nvim
+    ];
+  };
+  xdg.configFile.nvim = {
+    recursive = true;
+    source = ./nvim;
+  };
+}

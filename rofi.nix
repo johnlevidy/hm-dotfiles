9c563cf6c614f7e9f2545090507feaa8a118ddbb
John <john.levidy@gmail.com>
2025-01-13 22:21:51 -0500

    Rofi configuration, prepping background image, todo updates

--- rofi.nix
+++ rofi.nix
@@ -1,8 +1,15 @@
+# TODO this has to do the same thing as the i3 config, not keep a separate file
 { config, pkgs, ... }:
 {
   programs.rofi = {
     enable = true;
-    font = "Droid Sans Mono 28";
-    theme = "gruvbox-dark-soft";
-  };
+    font = "Droid Sans Mono 24";
+    theme = "/home/john/hm-dotfiles/rofi_config.rasi";
+    # Using extraConfig to append settings
+    extraConfig = {
+      drun-display-format = "{icon} {name}";
+      show-icons = true;
+      yoffset = 200;
+    };
+  }; 
 }
